8a416e245febbf043858bd4fd2301a1d57fc5e6d
escariche <javiescariz.je@gmail.com>
2016-04-28 00:55:21 +0200

    Entrega primera con cosas por completar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,13 +3,15 @@
 //GET /quizes/question
 exports.question = function(req, res, next){
 
-	var resp = req.query.answer || '';
-	res.render('quizes/question', {question: 'Capital de Italia'});
+	var answer = req.query.answer || '';
+	res.render('quizes/question', {question: 'Capital de Italia', answer: answer});
 };
 
 //GET /quizes/answer
 
 exports.check = function(req,res, next){
+	var answer = req.query.answer || "";
+
 	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
-	res.render('quizes/result', {result: result});
+	res.render('quizes/result', {result: result, answer: answer});
 };
